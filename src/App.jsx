5df5fb4cb33fba27578cdf5220fde27984f91b9d
Shadq <fmeda2104@gmail.com>
2023-11-04 09:40:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div className="bg-black">
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
